86b9ef071a186982b534c8697d2c5d04dd1aa288
Gena1 <genemodifiedfish4@gmail.com>
2016-01-02 23:15:04 +0300

    Commit

---
+++ BankingSite.UITests/CreditCardApplicationTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using BankingSite.UITests.DemoStuff;
+using NUnit.Framework;
+using WatiN.Core;
+
+namespace BankingSite.UITests
+{
+
+    /// <summary>
+    ///  The tests in this class are simple scripts and don't use the POM or LFM styles.
+    ///  As the number of tests grows, this simplistic style starts to create maintenance problems
+    /// </summary>
+
+    [TestFixture]    
+    public class CreditCardApplicationTests
+    {
+        [Test]
+        [STAThread]
+        public void ShouldShowSuccessPageForValidApplication()
+        {
+            using (var browser =
+                new IE("http://localhost:62727/Pages/ApplyForCreditCard.aspx"))
+            {
+                // Leave the browser open after the test completes
+                // to see what's happened for demo purposes
+                browser.AutoClose = false;
+                BrowserDemoHelper.BringToFront(browser);
+
+                browser.TextField(Find.ById("Name")).TypeText("Jason");
+                browser.TextField(Find.ById("Age")).TypeText("30");
+                browser.TextField(Find.ById("AirlineRewardNumber")).TypeText("A1234567");
+
+                browser.Button(Find.ById("ApplyNow")).Click();
+
+                Assert.That(browser.Url.Contains("ApplicationAccepted.aspx"));
+            }
+        }
+
+
+        [Test]
+        [STAThread]
+        public void ShouldShowCorrectApplicantDetailsOnSuccessPage()
+        {
+            using (var browser =
+                new IE("http://localhost:62727/Pages/ApplyForCreditCard.aspx"))
+            {
+                // Leave the browser open after the test completes
+                // to see what's happened for demo purposes
+                BrowserDemoHelper.BringToFront(browser);
+                browser.AutoClose = false;
+
+                browser.TextField(Find.ById("Name")).TypeText("Jason");
+                browser.TextField(Find.ById("Age")).TypeText("30");
+                browser.TextField(Find.ById("AirlineRewardNumber")).TypeText("A1234567");
+
+                browser.Button(Find.ById("ApplyNow")).Click();
+
+                var name = browser.Para(Find.ById("Name")).Text;
+
+                Assert.That(name, Is.EqualTo("Jason"));
+            }
+        }
+
+    }
+}
---
+++ BankingSite.UITests/PageObjectModelStyle/CreditCardApplicationTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NUnit.Framework;
+using WatiN.Core;
+
+namespace BankingSite.UITests.PageObjectModelStyle
+{
+    [TestFixture]    
+    public class CreditCardApplicationTests
+    {
+        [Test]
+        [STAThread]
+        public void ShouldShowSuccessPageForValidApplication()
+        {
+            using (var browser =
+                new IE("http://localhost:62727/Pages/ApplyForCreditCard.aspx"))
+            {
+                DemoStuff.BrowserDemoHelper.BringToFront(browser);
+                browser.AutoClose = false;
+                
+
+                var applyPage = browser.Page<ApplyForCreditCardPage>();
+                
+                applyPage.Name = "Jason";
+                //browser.TextField(Find.ById("Name")).TypeText("Jason");
+                applyPage.Age = "30";
+                //browser.TextField(Find.ById("Age")).TypeText("30");
+                applyPage.AirlineNumber = "A1234567";
+                //browser.TextField(Find.ById("AirlineRewardNumber")).TypeText("A1234567");
+
+                applyPage.ClickApplyButton();
+                //browser.Button(Find.ById("ApplyNow")).Click();
+
+                Assert.That(browser.Url.Contains("ApplicationAccepted.aspx"));
+            }
+        }
+
+
+        [Test]
+        [STAThread]
+        public void ShouldShowCorrectApplicantDetailsOnSuccessPage()
+        {
+              using (var browser =
+                new IE("http://localhost:62727/Pages/ApplyForCreditCard.aspx"))
+            {
+                DemoStuff.BrowserDemoHelper.BringToFront(browser);
+                browser.AutoClose = false;
+
+
+                var applyPage = browser.Page<ApplyForCreditCardPage>();
+                
+                applyPage.Name = "Jason";
+                applyPage.Age = "30";
+                applyPage.AirlineNumber = "A1234567";
+                applyPage.ClickApplyButton();
+
+                var acceptedPage = browser.Page<AcceptedPage>();
+
+                Assert.That(acceptedPage.Document.Url.Contains("ApplicationAccepted.aspx"));
+
+                Assert.That(acceptedPage.Name, Is.EqualTo("Jason"));
+            }
+        }
+
+    }
+}
